b92c4c41fdb04fc0e3b338e5d60cc02c146c580e
wahabdiyal <rana.wahab.146@gmail.com>
2021-04-08 17:43:10 +0500

    dev commit 1

--- database/migrations/2021_04_06_100355_create_links_table.php
+++ database/migrations/2021_04_06_100355_create_links_table.php
@@ -15,6 +15,7 @@ class CreateLinksTable extends Migration
     {
         Schema::create('links', function (Blueprint $table) {
             $table->id();
+            $table->string('url');
             $table->timestamps();
         });
     }
