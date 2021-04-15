c4c92b3349d4c49545f471cf660f3dfa1cc84402
wahabdiyal <rana.wahab.146@gmail.com>
2021-04-15 15:20:51 +0500

    dev commit 2

---
+++ app/Http/Controllers/DominController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Domin;
+use Illuminate\Http\Request;
+
+class DominController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Domin  $domin
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Domin $domin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Domin  $domin
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Domin $domin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Domin  $domin
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Domin $domin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Domin  $domin
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Domin $domin)
+    {
+        //
+    }
+}
